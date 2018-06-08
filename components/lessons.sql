9074901baf6af579f37bcf2a66ba0ca2cabc0b94
Sujith Srivardhan Arram <suji.srivardhan@gmail.com>
2018-06-08 14:24:23 -0400

    adding clean commit

--- components/lessons.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `lesson` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT NULL,
-  `module_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK8ig5gt5ec58weexave6kdn2cv` (`module_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
